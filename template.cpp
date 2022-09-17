92ecbbc80545ea3cb48ae9ff853743466e1dcddf
Lim-it <95406377+Lim-it@users.noreply.github.com>
2022-09-17 15:49:28 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define forn(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define rforn(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define in(A, B, C) assert( B <= A && A <= C)
+#define mp make_pair
+#define pb push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+const double pi=acos(-1.0);
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mapii;
+typedef set<int> seti;
+typedef long int li;
+typedef unsigned long int uli;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+
+/******** own func *******/
+
+void solve(){
+    cout << "troll";
+    return;
+}
+
+/******* main func *******/
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+	int tc;
+	cin >> tc;
+
+	while(tc--){
+		solve();
+	}
+	return 0;
+}
