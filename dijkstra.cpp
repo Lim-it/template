0f00b77f984a71b07515070b1d267afae963f2d9
Limit <95406377+Lim-it@users.noreply.github.com>
2023-01-14 20:33:24 +0800

    Update dijkstra.cpp
    for easier use

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,10 +4,11 @@
     //store each edge in the index of the source, first is end point, second is weight 
     
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-    vector<int> d(N, INF);
-    vector<bool> vis(N, false);
-    pq.push({0, 0});
-    d[0] = 0;
+    //int INF = 2e9;
+    //vector<int> d(N, INF);  <- uncomment it if needed
+    vector<bool> vis(n, false);
+    pq.push({0, start);
+    d[start] = 0;
 
     while (!pq.empty()) {
         int u = pq.top().second;
