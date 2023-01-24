47c4a8682080f9469082759162e6c5537fb0d818
Limit <95406377+Lim-it@users.noreply.github.com>
2023-01-24 16:55:09 +0800

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,65 @@
+//lowest common ancestor on tree
+
+struct LCA {
+    vector<int> height, euler, first, segtree;
+    vector<bool> visit;
+    int n;
+
+    LCA(vector<vector<int>> &adj, int root = 0) {
+        n = adj.size();
+        height.resize(n);
+        first.resize(n);
+        euler.reserve(n * 2);
+        visit.assign(n, false);
+        dfs(adj, root);
+        int m = euler.size();
+        segtree.resize(m * 4);
+        build(1, 0, m - 1);
+    }
+
+    void dfs(vector<vector<int>> &adj, int x, int h = 0) {
+        visit[x] = true;
+        height[x] = h;
+        first[x] = euler.size();
+        euler.push_back(x);
+        for (int y : adj[x]) {
+            if (!visit[y]) {
+                dfs(adj, y, h + 1);
+                euler.push_back(x);
+            }
+        }
+    }
+
+    void build(int node, int b, int e) {
+        if (b == e) {
+            segtree[node] = euler[b];
+        } else {
+            int mid = (b + e) / 2;
+            build(node << 1, b, mid);
+            build(node << 1 | 1, mid + 1, e);
+            int l = segtree[node << 1], r = segtree[node << 1 | 1];
+            segtree[node] = (height[l] < height[r]) ? l : r; //faster than min
+        }
+    }
+
+    int query(int node, int b, int e, int L, int R) {
+        if (b > R || e < L)
+            return -1;
+        if (b >= L && e <= R)
+            return segtree[node];
+        int mid = (b + e) >> 1;
+
+        int left = query(node << 1, b, mid, L, R);
+        int right = query(node << 1 | 1, mid + 1, e, L, R);
+        if (left == -1) return right;
+        if (right == -1) return left;
+        return height[left] < height[right] ? left : right;
+    }
+
+    int lca(int u, int v) {
+        int left = first[u], right = first[v];
+        if (left > right)
+            swap(left, right);
+        return query(1, 0, euler.size() - 1, left, right);
+    }
+};
